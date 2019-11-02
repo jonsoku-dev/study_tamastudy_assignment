d0f64fda67968345fc98814dd1d14400c15fbeec
jgam <jgam@alumni.nd.edu>
2019-11-02 10:42:18 +0900

    [calendar] holiday api in progress

---
+++ week4/jgam/react_calendar/src/hopiday.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Holiday extends Component{
+    constructor() {
+        super();
+        this.state={
+            holidays: [],
+        };
+    }
+}
+
