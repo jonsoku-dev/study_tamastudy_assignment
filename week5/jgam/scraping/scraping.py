99797ff6829a5ed081f35370f5aeee1cea40a876
jgam <jgam@alumni.nd.edu>
2019-11-05 20:12:55 +0900

    done

--- week5/jgam/scraping/scraping.py
+++ week5/jgam/scraping/scraping.py
@@ -1,1 +1,14 @@
 # This file enables scraping in foreground.
+import requests
+#import urllib.request
+import time
+from bs4 import BeautifulSoup
+
+url = 'https://www.skyscanner.jp/?previousCultureSource=GEO_LOCATION&redirectedFrom=www.skyscanner.net&locale=en-US'
+response = requests.get(url)
+
+print(response)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+print(soup)
